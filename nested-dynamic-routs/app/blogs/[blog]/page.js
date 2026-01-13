cf628d6912c105716ff573ea3156003413122e3b
Anil kumar <9012kumaranil@gmail.com>
2026-01-13 20:32:30 +0530

    nested-dynamic-routs

---
+++ nested-dynamic-routs/app/blogs/[blog]/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+async function Blog({params}) {
+    const {blog} = await params;
+  return (
+    <div>
+      <h1>Blog {blog}</h1>
+    </div>
+  )
+}
+
+export default Blog
